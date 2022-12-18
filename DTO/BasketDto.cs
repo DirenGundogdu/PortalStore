0ed8579016684bb14147d0f5bcc559a6952bf8c5
DirenGundogdu <gundogdudiren@gmail.com>
2022-12-18 22:36:47 +0300

    Change project name

--- DTO/BasketDto.cs
+++ DTO/BasketDto.cs
@@ -4,8 +4,8 @@ public class BasketDto
 {
     public int Id { get; set; }
     public int ProductId { get; set; }
-    public ProductDto Product { get; set; }
+    public ProductDto? Product { get; set; }
     public int CustomerId { get; set; }
-    public CustomerDto Customer { get; set; }
+    public CustomerDto? Customer { get; set; }
     public int Quantity { get; set; }
 }
